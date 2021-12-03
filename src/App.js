b2356c3cc7195d23d8d33e4c353bebd0569cc2fa
rahafidz1 <raihanabdulhafidz1@gmail.com>
2021-12-03 10:55:28 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Homepage from "../src/pages/homepage/homepage.component";
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      {/* <Route exact path="/" component={Homepage}></Route>
+      <Route path="/hats" component={HatsPage}></Route> */}
+      <Homepage/>
     </div>
   );
 }
